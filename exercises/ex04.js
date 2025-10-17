526f14854cf3aed7a4be165dbca243dedad1278d
ACEDOG2002 <asadeocio@gmail.com>
2025-10-16 18:35:13 -0700

    Han Solo

--- exercises/ex04.js
+++ exercises/ex04.js
@@ -1,28 +1,19 @@
+let count = 0;
+let colors = ["Orchid", "Coral", "HotPink", "Plum"]; // new line tp add
 
-let totalNumber = 0;
+$("#needy-button").click(function () {
+    
+    let reminder=count % colors.length;
 
-// add a button titled click me
+    $("#needy-button").html( "Clicks: " + count + " Color: " + colors[count] ); 
 
-$("#needy-button").click(function(){
+    $("body").css("background-color", colors[count]);
+    
+    $("body").css("background-color", colors [reminder]);
+    
+    
+    count = count + 1;
 
-    totalNumber = totalNumber + 3;
 
-    let sentence = "this is it you entered";
-    let totalNumbers= sentence + totalNumber;
-
-    $("#needy-button").html(totalNumbers);
 
 });
-
-
-//when it is clicked 
-//add three to the total number
-
-//show the total number
-//on our button
-//show "clicked total number times"
-
-
-
-//a top limit
-
---
+++ exercises/lab4.js
@@ -0,0 +1,14 @@
+// declaring an array with name myCommutes
+let myCommutes = ["metro bus", "UCSC taps bus", "personal bike"];
+// declaring an object with name myFavouriteCommute
+let myFavouriteCommute = {
+    type: "personal bike",
+    dirt path: 1,
+    print: "To my Introduction to Computer Programming Class",
+    isMountainBike: true,
+    drivers: ["Asa"],
+};
+let megaSentence;
+megaSentence = "<p>My two top commutes from the array are: "+MyComutes[personal bike]+", "+MyComutes[metro bus]+"</p>";
+megaSentence = megaSentence + "<p>My favourite commute possesses such characteristics: type - " +personal bike+ ", dirt path 2 " + myFavouriteCommute.dirtpath + ", the best driver:Asa " + myFavouriteCommute.drivers[0] + "</p>";
+$("#output").html(megaSentence)
